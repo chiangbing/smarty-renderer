eb677e6b86f1e0c43d79335e31648fe312173e3f
bingjiang <bingjiang@tencent.com>
2021-01-12 20:01:45 +0800

    add: add register_package function

---
+++ plugins/function.register_package.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Allows to call CClientScript->registerPackage.
+ *
+ * Syntax:
+ * {register_package name="test"}
+ *
+ * @see CHtml::link().
+ *
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ */
+function smarty_function_register_package($params, &$smarty) {
+    if (!isset($params['name'])) {
+        throw new CException(Yii::t('yiiext', "You should specify name parameter."));
+    }
+    $name = $params['name'];
+    Yii::app()->clientScript->registerPackage($name);
+}
